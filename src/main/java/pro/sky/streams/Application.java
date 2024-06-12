8dfa8404e764e6ffc83422cbd0fd8033cb34937c
Ed <eduard@kamenskikh.me>
2024-06-13 02:44:51 +0400

    Initial commit

--- src/main/java/pro/sky/streams/Application.java
+++ src/main/java/pro/sky/streams/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Block2Hw9StreamsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Block2Hw9StreamsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
